64d84ea59972eddd1d90106c7b09ce1f9623d697
gcz <gczgcz0929@gmail.com>
2019-02-18 12:18:42 +0800

    创建用户表

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,15 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            // 完善用户(老师)字段 
+            $table->string('avatar'); // 头像
+            $table->string('mobile'); // 手机
+            // $table->string('wx_qrcode'); // 微信二维码
+            $table->string('person_title'); // 头衔
+            $table->string('idcard'); // 身份证号
+            $table->string('id_font'); // 身份证正面
+            $table->string('id_back'); // 身份证反面
+
             $table->rememberToken();
             $table->timestamps();
         });
